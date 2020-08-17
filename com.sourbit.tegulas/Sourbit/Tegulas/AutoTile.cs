7e1175996e69ba2a2824daa9e2af6e775f4ad17d
NemoStein <contato@nemostein.com.br>
2020-08-16 21:04:39 -0300

    Initial Commit

---
+++ com.sourbit.tegulas/Sourbit/Tegulas/AutoTile.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Sourbit.Tegulas
+{
+    [CreateAssetMenu(fileName = "AutoTile", menuName = "Sourbit/Tegulas AutoTile")]
+    public class AutoTile : TileBase
+    {
+        public Texture2D SourceTexture;
+
+        Dictionary<int, Sprite> Tiles;
+
+        override public void RefreshTile(Vector3Int location, ITilemap tilemap)
+        {
+            for (var y = -1; y <= 1; y++)
+            {
+                for (var x = -1; x <= 1; x++)
+                {
+                    var position = new Vector3Int(location.x + x, location.y + y, location.z);
+                    if (IsAutoTile(tilemap, position))
+                    {
+                        tilemap.RefreshTile(position);
+                    }
+                }
+            }
+        }
+
+        override public void GetTileData(Vector3Int location, ITilemap tilemap, ref TileData tileData)
+        {
+            Prepare();
+
+            var mask = 0;
+
+            if (IsAutoTile(tilemap, location + Vector3Int.up))
+            {
+                mask += Places.X1_Y2;
+            }
+
+            if (IsAutoTile(tilemap, location + Vector3Int.down))
+            {
+                mask += Places.X1_Y0;
+            }
+
+            if (IsAutoTile(tilemap, location + Vector3Int.left))
+            {
+                mask += Places.X0_Y1;
+            }
+
+            if (IsAutoTile(tilemap, location + Vector3Int.right))
+            {
+                mask += Places.X2_Y1;
+            }
+
+            if ((mask & (Places.X1_Y2 | Places.X0_Y1)) == (Places.X1_Y2 | Places.X0_Y1) && IsAutoTile(tilemap, location + Vector3Int.up + Vector3Int.left))
+            {
+                mask += Places.X0_Y2;
+            }
+
+            if ((mask & (Places.X1_Y2 | Places.X2_Y1)) == (Places.X1_Y2 | Places.X2_Y1) && IsAutoTile(tilemap, location + Vector3Int.up + Vector3Int.right))
+            {
+                mask += Places.X2_Y2;
+            }
+
+            if ((mask & (Places.X1_Y0 | Places.X0_Y1)) == (Places.X1_Y0 | Places.X0_Y1) && IsAutoTile(tilemap, location + Vector3Int.down + Vector3Int.left))
+            {
+                mask += Places.X0_Y0;
+            }
+
+            if ((mask & (Places.X1_Y0 | Places.X2_Y1)) == (Places.X1_Y0 | Places.X2_Y1) && IsAutoTile(tilemap, location + Vector3Int.down + Vector3Int.right))
+            {
+                mask += Places.X2_Y0;
+            }
+
+            var sprite = Tiles[mask];
+            tileData.sprite = sprite;
+        }
+
+        void Prepare()
+        {
+            if (Tiles == null)
+            {
+                Tiles = new Dictionary<int, Sprite>();
+                var all = AssetDatabase.LoadAllAssetRepresentationsAtPath(AssetDatabase.GetAssetPath(this));
+                foreach (var item in all)
+                {
+                    if (item.name.StartsWith("Sprite"))
+                    {
+                        var key = Convert.ToInt16(item.name.Split(' ')[1]);
+                        Tiles.Add(key, item as Sprite);
+                    }
+                }
+            }
+        }
+
+        bool IsAutoTile(ITilemap tilemap, Vector3Int position)
+        {
+            return tilemap.GetTile(position) == this;
+        }
+    }
+
+    public struct Places
+    {
+        public const int ALL = 0b11111111;
+        public const int X0_Y0 = 0b00000001;
+        public const int X1_Y0 = 0b00000010;
+        public const int X2_Y0 = 0b00000100;
+        public const int X0_Y1 = 0b00001000;
+        public const int X2_Y1 = 0b00010000;
+        public const int X0_Y2 = 0b00100000;
+        public const int X1_Y2 = 0b01000000;
+        public const int X2_Y2 = 0b10000000;
+    }
+}
---
+++ com.sourbit.tegulas/Sourbit/Tegulas/AutoTileEditor.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Sourbit.Tegulas
+{
+    [CustomEditor(typeof(AutoTile))]
+    public class AutoTileEditor : Editor
+    {
+        override public void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("Build Tileset"))
+            {
+                BuildTileset();
+            }
+        }
+
+        void BuildTileset()
+        {
+            var targetAutoTile = (target as AutoTile);
+            var assetPath = AssetDatabase.GetAssetPath(targetAutoTile);
+            var autoTile = CreateInstance<AutoTile>();
+
+            var sourceTexture = targetAutoTile.SourceTexture;
+            autoTile.SourceTexture = sourceTexture ?? throw new Exception("No Source Texture");
+
+            AssetDatabase.DeleteAsset(assetPath);
+            AssetDatabase.CreateAsset(autoTile, assetPath);
+
+            if (sourceTexture.height % 2 == 1)
+            {
+                throw new Exception($"Tile side (width and height) must be multiple of 2 (even). Odd tile height: {sourceTexture.height}");
+            }
+
+            if (sourceTexture.height != sourceTexture.width / 5f)
+            {
+                throw new Exception($"Tile must be square. Tile height: {sourceTexture.height}. Expected texture width: {sourceTexture.height * 5}.");
+            }
+
+            var TileSide = sourceTexture.height;
+            var TileHalfSide = TileSide / 2;
+            var minSize = TileSide * 7;
+            var powerOfTwo = 2;
+
+            while (powerOfTwo < minSize)
+            {
+                powerOfTwo *= 2;
+            }
+
+            var texture = new Texture2D(powerOfTwo, powerOfTwo, TextureFormat.RGBA32, false)
+            {
+                filterMode = FilterMode.Point
+            };
+
+            var slicesMap = new Dictionary<int, List<int>>();
+            var tileOffset = 0;
+
+            for (var mask = 0; mask <= Places.ALL; mask++)
+            {
+                if (!(
+                    ((~mask & Places.X1_Y0) != 0 && ((mask & Places.X0_Y0) != 0 || (mask & Places.X2_Y0) != 0)) ||
+                    ((~mask & Places.X0_Y1) != 0 && ((mask & Places.X0_Y0) != 0 || (mask & Places.X0_Y2) != 0)) ||
+                    ((~mask & Places.X2_Y1) != 0 && ((mask & Places.X2_Y0) != 0 || (mask & Places.X2_Y2) != 0)) ||
+                    ((~mask & Places.X1_Y2) != 0 && ((mask & Places.X0_Y2) != 0 || (mask & Places.X2_Y2) != 0))
+                ))
+                {
+                    var slices = SlicesFromBitmask(mask);
+
+                    for (var slice = 0; slice < slices.Length; slice++)
+                    {
+                        var sliceMask = (slices[slice] << 2) + slice;
+
+                        if (!slicesMap.ContainsKey(sliceMask))
+                        {
+                            slicesMap.Add(sliceMask, new List<int>());
+                        }
+
+                        slicesMap[sliceMask].Add(tileOffset);
+                    }
+
+                    var sprite = Sprite.Create(texture, new Rect(tileOffset % 7 * TileSide, tileOffset / 7 * TileSide, TileSide, TileSide), new Vector2(0.5f, 0.5f), TileSide);
+                    sprite.name = "Sprite " + mask;
+
+                    AssetDatabase.AddObjectToAsset(sprite, autoTile);
+
+                    tileOffset++;
+                }
+            }
+
+            foreach (var entry in slicesMap)
+            {
+                var sourceMask = entry.Key;
+
+                var tileMask = sourceMask >> 2 & 0b111;
+                var sliceMask = sourceMask & 0b11;
+
+                var sourceX = tileMask * TileSide + (sliceMask & 1) * TileHalfSide;
+                var sourceY = (sliceMask >> 1 & 1) * TileHalfSide;
+
+                var slicePixels = sourceTexture.GetPixels(sourceX, sourceY, TileHalfSide, TileHalfSide);
+
+                foreach (var targetMask in entry.Value)
+                {
+                    var sliceX = targetMask % 7 * TileSide + TileHalfSide * (sourceMask & 1);
+                    var sliceY = targetMask / 7 * TileSide + TileHalfSide * (sourceMask >> 1 & 1);
+
+                    texture.SetPixels(sliceX, sliceY, TileHalfSide, TileHalfSide, slicePixels);
+                }
+            }
+
+            texture.Apply();
+            texture.name = "Tileset Texture";
+
+            AssetDatabase.AddObjectToAsset(texture, assetPath);
+            AssetDatabase.ImportAsset(assetPath);
+        }
+
+        int[] SlicesFromBitmask(int bitmask)
+        {
+            return new int[]
+            {
+              SliceFromBits((bitmask & Places.X1_Y0) != 0, (bitmask & Places.X0_Y1) != 0, (bitmask & Places.X0_Y0) != 0),
+              SliceFromBits((bitmask & Places.X1_Y0) != 0, (bitmask & Places.X2_Y1) != 0, (bitmask & Places.X2_Y0) != 0),
+              SliceFromBits((bitmask & Places.X1_Y2) != 0, (bitmask & Places.X0_Y1) != 0, (bitmask & Places.X0_Y2) != 0),
+              SliceFromBits((bitmask & Places.X1_Y2) != 0, (bitmask & Places.X2_Y1) != 0, (bitmask & Places.X2_Y2) != 0)
+            };
+        }
+
+        int SliceFromBits(bool vertical, bool horizontal, bool corner)
+        {
+            if (corner) return 4;
+            if (vertical && horizontal) return 3;
+            if (horizontal) return 2;
+            if (vertical) return 1;
+            return 0;
+        }
+    }
+}
